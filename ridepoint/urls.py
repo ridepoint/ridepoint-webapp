9cbb2bb4ca75acfd3f1ee1f4e0e5cc43408754b1
Francis Carlo Tadena <franciscarlotadena@yahoo.com.ph>
2022-12-24 16:38:07 +0800

    2412022

--- ridepoint/urls.py
+++ ridepoint/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-#test commit code changes
+#test commit code changes, test
 
 from django.contrib import admin
 from django.urls import path
