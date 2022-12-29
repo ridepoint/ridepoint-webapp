aad2b637a01945df0951824db80bb4303768167f
Francis Carlo Tadena <franciscarlotadena@yahoo.com.ph>
2022-12-29 23:28:13 +0800

    Updated DATABASE to MySql

--- ridepoint/settings.py
+++ ridepoint/settings.py
@@ -74,12 +74,25 @@ WSGI_APPLICATION = 'ridepoint.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rp_db1',
+        'USER': 'root',
+        'PASSWORD': 'Gat#105strike',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
+
 
 
 # Password validation
